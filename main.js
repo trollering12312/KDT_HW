33bbb2370fa7c05ecd4f9c84dfe3909863a821a2
trollering12312 <trollering12312@gmail.com>
2021-08-06 16:52:53 +0900

    refactor: using filter instead of map

--- main.js
+++ main.js
@@ -12,69 +12,75 @@ const data = [
     { name: '껌', price: 500 }
 ];
 
-//const -> let
-let line;
-let amount;
-
+// 입력 최소값
 const minPrice = 500
 
-// 사용자 입력 받기
 while(1){
 
-    line = prompt('최대 금액을 입력해주세요.');
-    amount = Number(line); // string -> number
+    const line = prompt('최대 금액을 입력해주세요.(그만하려면 q 입력)');
+    const amount = Number(line); // string -> number
 
     if(!Number.isNaN(amount)){//num check
-        break;
+        const item = getItemByAmount(amount);
+
+        const msg = item ? 
+            `${amount}원으로 살 수 있는 가장 비싼 상품은 [${item.name}]이고, 가격은 ${item.price}원입니다.` : 
+            '살 수 있는 상품이 없습니다.';
+
+        // 결과 출력
+        alert(msg);
     }
     else{
+
+        if(line == 'q' || line == 'Q')
+            break
+        
         alert('숫자를 입력해주세요!');
+
     }
 }
 
 
-// 주어진 금액으로 살 수 있는 가장 비싼 상품을 구함
-const item = getItemByAmount(amount);
-
-const msg = item ? 
-    `${amount}원으로 살 수 있는 가장 비싼 상품은 [${item.name}]이고, 가격은 ${item.price}원입니다.` : 
-    '살 수 있는 상품이 없습니다.';
-
-// 결과 출력
-alert(msg);
-
 // 아래에 getItemByAmount 함수를 작성하세요.
 function getItemByAmount(amount){
 
-    //돈이 부족한 경우
-    if(amount<minPrice)
-        return null;
+    const filtered_list = data.filter(item => item.price >= minPrice && item.price <= amount)
+    
+    const sorted_list = filtered_list.sort((a,b) => a.price - b.price) // 객체의 price 속성 비교
 
-    //살수 있는 항목중 가장 비싼 값 계산
-    tempArray = data.map(arg => +arg.price); //price로만 이루어진 array 생성
-    result = 0;
+    console.log(sorted_list)
 
-    while(1){
+    return sorted_list[sorted_list.length-1]
 
-        if(tempArray.length === 0){ //array가 텅빔 = 살게 없음
-            return ;
-        }
+    // //돈이 부족한 경우
+    // if(amount<minPrice)
+    //     return null;
+
+    // //살수 있는 항목중 가장 비싼 값 계산
+    // tempArray = data.map(arg => +arg.price); //price로만 이루어진 array 생성
+    // result = 0;
+
+    // while(1){
+
+    //     if(tempArray.length === 0){ //array가 텅빔 = 살게 없음
+    //         return ;
+    //     }
         
-        max = Math.max.apply(Math, tempArray);//max를 찾고 보유금과 비교
+    //     max = Math.max.apply(Math, tempArray);//max를 찾고 보유금과 비교
 
-        if(amount >= max){
-            result = max;
-            break;
-        }
+    //     if(amount >= max){
+    //         result = max;
+    //         break;
+    //     }
 
-        //array에서 필요없는 항목 삭제
-        tempArray.splice(tempArray.indexOf(max),1)
+    //     //array에서 필요없는 항목 삭제
+    //     tempArray.splice(tempArray.indexOf(max),1)
 
-    }
+    // }
 
-    let _index = data.findIndex(arg => arg.price === result); // result의 index 찾아서 객체 return
+    // let _index = data.findIndex(arg => arg.price === result); // result의 index 찾아서 객체 return
 
-    return data[_index];
+    // return data[_index];
 
 }
 
